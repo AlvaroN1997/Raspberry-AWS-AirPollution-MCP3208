b7b5a8ae03693c77fd744b415b4bea1a1f0bb4ed
AlvaroN1997 <38118994+AlvaroN1997@users.noreply.github.com>
2019-11-10 17:27:39 -0500

    Update CalibracionPPM.py
    Added new sensors, MQ131 and MQ136

--- CalibracionPPM.py
+++ CalibracionPPM.py
@@ -54,13 +54,12 @@ SPICLK = 11
 SPIMISO = 9
 SPIMOSI = 10
 SPICS = 8
-mq2_apin = 0
-mq5_apin = 1
-mq7_apin = 2
+mq5_apin = 0
+mq7_apin = 1
+mq131_apin = 2
 mq135_apin = 3
+mq136_apin = 4
 RL = 1
-DHT_SENSOR = Adafruit_DHT.DHT22
-DHT_PIN = 4
 
 #Here we inicialize the GPIO, so we can use the connectors of the raspberry to the MCP
 def init():
@@ -129,13 +128,15 @@ def calculatePromRS(analogRead):
 def obtainPPM(ratio_rs_ro, id_mq):
     try:
         if id_mq == 0:
-            return (pow(10, ( (math.log(ratio_rs_ro)-MQ2Curve[1]) / MQ2Curve[2]) + MQ2Curve[0]))
-        elif id_mq == 1:
             return (pow(10, ( (math.log(ratio_rs_ro)-MQ5Curve[1]) / MQ5Curve[2]) + MQ5Curve[0]))
-        elif id_mq == 2:
+        elif id_mq == 1:
             return (pow(10, ( (math.log(ratio_rs_ro)-MQ7Curve[1]) / MQ7Curve[2]) + MQ7Curve[0]))
+        elif id_mq == 2:
+            return (pow(10, ( (math.log(ratio_rs_ro)-MQ131Curve[1]) / MQ131Curve[2]) + MQ131Curve[0]))
         elif id_mq == 3:
             return (pow(10, ( (math.log(ratio_rs_ro)-MQ135Curve[1]) / MQ135Curve[2]) + MQ135Curve[0]))
+        elif id_mq == 4:
+        	return (pow(10, ( (math.log(ratio_rs_ro)-MQ136Curve[1]) / MQ136Curve[2]) + MQ136Curve[0]))
         else:
             return 0
     except ValueError:
@@ -148,46 +149,52 @@ def main():
     f = open("lecturasPPM.txt", "+w")
     
     while True:
-        # Using the DHT22 sensor
-        humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-        
         # This instances are the analogReads, in other words, their values are between 0 to 1023
-        MQ2 = readadc(mq2_apin, SPICLK, SPIMOSI, SPIMISO, SPICS)
         MQ5 = readadc(mq5_apin, SPICLK, SPIMOSI, SPIMISO, SPICS)
         MQ7 = readadc(mq7_apin, SPICLK, SPIMOSI, SPIMISO, SPICS)
+        MQ131 = readadc(mq131_apin, SPICLK, SPIMOSI, SPIMISO, SPICS)
         MQ135 = readadc(mq135_apin, SPICLK, SPIMOSI, SPIMISO, SPICS)
-    
+    	MQ136 = readadc(mq136_apin, SPICLK, SPIMOSI, SPIMISO, SPICS)
+
         # Now we calculte only the RS, the RO has already been calculated
-        MQ2rs = calculatePromRS(MQ2)
         MQ5rs = calculatePromRS(MQ5)
         MQ7rs = calculatePromRS(MQ7)
+        MQ131rs = calculatePromRS(MQ131)
         MQ135rs = calculatePromRS(MQ135)
+        MQ136rs = calculatePromRS(MQ136)
         
         # Now after we have the RS and RO, we start calculating the PPM values of each sensor
-        ppm_mq2 = obtainPPM(MQ2rs/MQ2ro, mq2_apin)
         ppm_mq5 = obtainPPM(MQ5rs/MQ5ro, mq5_apin)
         ppm_mq7 = obtainPPM(MQ7rs/MQ7ro, mq7_apin)
+        ppm_mq131 = obtainPPM(MQ131rs/MQ131ro, mq131_apin)
         ppm_mq135 = obtainPPM(MQ135rs/MQ135ro, mq135_apin)
+        ppm_mq136 = obtainPPM(MQ136rs/MQ136ro, mq136_apin)
         
         # Now we print the data
         #if humidity is not None and temperature is not None:
         #    print("Temperature: {0:0.1f}*C".format(temperature))
         #    print("Humidity: {}%".format(humidity))
-        print("SMOKE: " + str(ppm_mq2) + " ppm")
         print("LPG: " + str(ppm_mq5) + " ppm")
         print("CO: " + str(ppm_mq7) + " ppm")
+        print("NO2: " + str(ppm_mq131) + " ppm")
         print("NH4: " + str(ppm_mq135) + " ppm")
+        print("SO2: " + str(ppm_mq136) + " ppm")
         print(datetime.datetime.now())
         print("***************************")
         
         # Here we add the information in the text file
-        f.write("SMOKE: " + str(ppm_mq2) + " ppm")
         f.write("LPG: " + str(ppm_mq5) + " ppm")
         f.write("CO: " + str(ppm_mq7) + " ppm")
+        f.write("NO2: " + str(ppm_mq131) + " ppm")
         f.write("NH4: " + str(ppm_mq135) + " ppm")
+        f.write("SO2: " + str(ppm_mq136) + " ppm")
+        f.write(datetime.datetime.now())
         f.write("***************************")
         
-        message = {
+        """
+		BackUp
+
+		message = {
             "device": client_id,
             "location": location,
             "mq2": int(ppm_mq2),
@@ -200,21 +207,39 @@ def main():
         }
         send_message(client=mqtt_client, data=message)
         print("Published topic %s: %s\n" % (topic, message))
+        """
+
+        message = {
+            "device": client_id,
+            "location": location,
+            "mq5": int(ppm_mq5),
+            "mq7": int(ppm_mq7),
+            "mq131": int(ppm_mq131),
+            "mq135": int(ppm_mq135),
+            "mq136": int(ppm_mq136),
+            "issued_date": datetime.datetime.now().timestamp(),
+            "metadata": {"other": "value"},
+        }
+        send_message(client=mqtt_client, data=message)
+        print("Published topic %s: %s\n" % (topic, message))
         
         time.sleep(1.0)
 
-# Each sensor (MQ2,5,7 and 135) has their own sensibility to a controlled area, this one is air. Also you can see the values on the datasheets
-air = [9.83, 6.50, 26.95, 3.12] #Used to calculate RO
+# Each sensor (MQ5,7,131,135 and 136) has their own sensibility to a controlled area, this one is air. Also you can see the values on the datasheets
+air = [6.50, 26.95, 8.75, 3.12, 8.75] #Used to calculate RO
 
-MQ2Curve = [2.30,0.53,-0.44]
 MQ5Curve = [2.30,-0.15,-0.37]
 MQ7Curve = [1.70,0.24,-0.677]
+MQ131Curve = [0.70,0.95,-0.3249]
 MQ135Curve = [1.00,0.46,-0.235]
+MQ136Curve = [0.70,0.799,-0.3845]
 
-MQ2ro = 12.91
 MQ5ro = 10.36
 MQ7ro = 0.63
+MQ131ro = 20 #Por definir
 MQ135ro = 19.37
+MQ136ro = 20 #Por definir
+
 
 if __name__ =='__main__':
     """Execute send payload."""
